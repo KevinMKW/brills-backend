70aa844811e94ab453c6bdb22924c81e7e2b62e6
KevinMKW <mkwkvn@gmail.com>
2023-12-30 12:12:52 +0300

    Add Updated modifications

--- brills/settings.py
+++ brills/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'brills',
+    'brills_backend',
 ]
 
 MIDDLEWARE = [
--- brills_backend/models.py
+++ brills_backend/models.py
@@ -15,7 +15,7 @@ class Expense(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField()
-    cost = models.DecimalField(maxx_digits=10, decimal_places=2)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return f"{self.name} - {self.cost}"
