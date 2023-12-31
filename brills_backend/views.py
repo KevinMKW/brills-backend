a3d32b2571989ff1067a9f1aa4a03db0f85b51d3
KevinMKW <mkwkvn@gmail.com>
2023-12-31 18:22:06 +0300

    Update views

--- brills_backend/views.py
+++ brills_backend/views.py
@@ -1,60 +1,20 @@
-from json import JSONDecodeError
-from django.http import JsonResponse
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Profile, Bill
 from .serializers import ProfileSerializer, BillSerializer
-from rest_framework.parsers import JSONParser
-from rest_framework import views, status
-from rest_framework.response import Response
 
-class ProfileAPIView(views.APIView):
+class ProfileAPIView(ListCreateAPIView):
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
-    def get_serializer_context(self):
-        return {
-            'request': self.request,
-            'format': self.format_kwarg,
-            'view': self
-        }
-
-    def get_serializer(self, *args, **kwargs):
-        kwargs['context'] = self.get_serializer_context()
-        return self.serializer_class(*args, **kwargs)
-
-    def post(self, request):
-        try:
-            data = JSONParser().parse(request)
-            serializer = ProfileSerializer(data=data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except JSONDecodeError:
-            return JsonResponse({"result": "error","message": "Json decoding error"}, status= 400)
+class Profile(RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
-
-class BillAPIView(views.APIView):
+class BillAPIView(ListCreateAPIView):
+    queryset = Bill.objects.all()
     serializer_class = BillSerializer
 
-    def get_serializer_context(self):
-        return {
-            'request': self.request,
-            'format': self.format_kwarg,
-            'view': self
-        }
-
-    def get_serializer(self, *args, **kwargs):
-        kwargs['context'] = self.get_serializer_context()
-        return self.serializer_class(*args, **kwargs)
-
-    def post(self, request):
-        try:
-            data = JSONParser().parse(request)
-            serializer = BillSerializer(data=data)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data)
-            else:
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except JSONDecodeError:
-            return JsonResponse({"result": "error","message": "Json decoding error"}, status= 400)
+class Bill(RetrieveUpdateDestroyAPIView):
+    queryset = Bill.objects.all()
+    serializer_class = BillSerializer
