61e45b6c27f7125665d370441d1f78106b840fca
KevinMKW <mkwkvn@gmail.com>
2023-12-31 14:12:13 +0300

    Register models

--- brills_backend/admin.py
+++ brills_backend/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Bill
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'income', 'currency')
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'cost', 'color')
