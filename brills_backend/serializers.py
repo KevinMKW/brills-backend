9f4497b2de2a4e0d011ad75b2fa31ddd41147e40
KevinMKW <mkwkvn@gmail.com>
2023-12-31 14:09:53 +0300

    Add modifications in serializer

--- brills_backend/serializers.py
+++ brills_backend/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
 from rest_framework.fields import CharField, EmailField, DecimalField
-from .models import Profile, Expense
+from .models import Profile, Bill
 
 class ProfileSerializer(serializers.ModelSerializer):
     name = CharField(required=True)
     email = EmailField(required=True)
-    income = DecimalField(required=True)
+    income = DecimalField(max_digits=12, decimal_places=2, required=True)
     currency = CharField(required=True)
-    color = CharField(required=True)
 
     class Meta:
         model = Profile
@@ -15,19 +14,20 @@ class ProfileSerializer(serializers.ModelSerializer):
             'name',
             'email',
             'income',
-            'currency',
-            'color'
+            'currency'
         )
 
-class ExpenseSerializer(serializers.ModelSerializer):
+class BillSerializer(serializers.ModelSerializer):
     name = CharField(source="title", required=True)
     description = CharField(source="description", required=True)
-    cost = DecimalField(required=True)
+    cost = DecimalField(max_digits=12, decimal_places=2, required=True)
+    color = CharField(required=True)
 
     class Meta:
-        model = Expense
+        model = Bill
         fields = (
             'name',
             'description',
-            'cost'
+            'cost',
+            'color'
         )
