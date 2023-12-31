47d781ddc2de758fa1633abfb52c78534be38cf8
KevinMKW <mkwkvn@gmail.com>
2023-12-31 14:12:36 +0300

    Add updates

--- brills_backend/tests.py
+++ brills_backend/tests.py
@@ -2,52 +2,52 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
-from .models import UserProfile, Expense
+from .models import UserProfile, Bill
 from .serializers import UserProfileSerializer
 
-class ExpenseAPITests(TestCase):
+class BillAPITests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
         self.user = User.objects.create_user(username='testuser', password='testpass')
         self.user_profile = UserProfile.objects.create(user=self.user, email='test@example.com', name='Test User')
 
-        self.expense_data = {
+        self.Bill_data = {
             'user': self.user,
-            'name': 'Test Expense',
-            'description': 'This is a test expense.',
+            'name': 'Test Bill',
+            'description': 'This is a test Bill.',
             'cost': '50.00',
         }
-        self.expense = Expense.objects.create(**self.expense_data)
+        self.Bill = Bill.objects.create(**self.Bill_data)
 
-    def test_create_expense(self):
+    def test_create_Bill(self):
         new_username = 'testuser2'
         user = User.objects.create_user(username='new_uwername', password='testpass')
         user_serializer = UserProfileSerializer(user)
         serialized_user = user_serializer.data
 
-        self.expense_data['user'] = serialized_user
-        response = self.client.post('/api/expenses/', data=self.expense_data, format='json')
+        self.Bill_data['user'] = serialized_user
+        response = self.client.post('/api/Bills/', data=self.Bill_data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Expense.objects.count(), 1)  
+        self.assertEqual(Bill.objects.count(), 1)  
 
-    def test_read_expense(self):
-        response = self.client.get('/api/expenses/')
+    def test_read_Bill(self):
+        response = self.client.get('/api/Bills/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1) 
 
-    def test_update_expense(self):
-        updated_data = {'name': 'Updated Expense', 'cost': '75.00'}
-        response = self.client.put(f'/api/expenses/{self.expense.id}/', data=updated_data, format='json')
+    def test_update_Bill(self):
+        updated_data = {'name': 'Updated Bill', 'cost': '75.00'}
+        response = self.client.put(f'/api/Bills/{self.Bill.id}/', data=updated_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.expense.refresh_from_db()
-        self.assertEqual(self.expense.name, 'Updated Expense')
-        self.assertEqual(str(self.expense.cost), '75.00')
+        self.Bill.refresh_from_db()
+        self.assertEqual(self.Bill.name, 'Updated Bill')
+        self.assertEqual(str(self.Bill.cost), '75.00')
 
-    def test_delete_expense(self):
-        response = self.client.delete(f'/api/expenses/{self.expense.id}/')
+    def test_delete_Bill(self):
+        response = self.client.delete(f'/api/Bills/{self.Bill.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Expense.objects.count(), 0)
+        self.assertEqual(Bill.objects.count(), 0)
 
 # You can add more tests for error cases, edge cases, etc.
