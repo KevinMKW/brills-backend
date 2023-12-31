6f32baec0d6f2f60bd4ed4dd98fc099512c3d362
KevinMKW <mkwkvn@gmail.com>
2023-12-31 14:11:51 +0300

    add modifications

--- brills_backend/models.py
+++ brills_backend/models.py
@@ -11,17 +11,17 @@ class Profile(TimeStampedModel, ActivatorModel, Model):
     email = models.EmailField()
     income = models.DecimalField(max_digits=12, decimal_places=2, null=True, blank=True)
     currency = models.CharField(max_length=10, null=True, blank=True)
-    color = models.CharField(max_length=7, null=True, blank=True, help_text="Hex color code, e.g., #RRGGBB")
 
 
     def __str__(self):
         return f"{self.name} - {self.currency}"
 
-class Expense(TitleDescriptionModel, Model):
+class Bill(TitleDescriptionModel, Model):
     class Meta:
         verbose_name_plural = "Expences"
 
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=7, null=True, blank=True, help_text="Hex color code, e.g., #RRGGBB")
 
     def __str__(self):
         return f"{self.title} - {self.cost}"
