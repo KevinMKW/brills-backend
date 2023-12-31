86adb48853bb76892a7b1d8e05235007e36afd32
KevinMKW <mkwkvn@gmail.com>
2023-12-31 13:27:38 +0300

    Update views to handle .get() and .post() request using APIView class

--- brills_backend/views.py
+++ brills_backend/views.py
@@ -1,11 +1,60 @@
-from rest_framework import generics
-from .models import UserProfile, Expense
-from .serializers import UserProfileSerializer, ExpenseSerializer
+from json import JSONDecodeError
+from django.http import JsonResponse
+from .serializers import ProfileSerializer, ExpenseSerializer
+from rest_framework.parsers import JSONParser
+from rest_framework import views, status
+from rest_framework.response import Response
 
-class UserProfileList(generics.ListCreateAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+class ProfileAPIView(views.APIView):
+    serializer_class = ProfileSerializer
 
-class ExpenseList(generics.ListCreateAPIView):
-    queryset = Expense.objects.all()
+    def get_serializer_context(self):
+        return {
+            'request': self.request,
+            'format': self.format_kwarg,
+            'view': self
+        }
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['context'] = self.get_serializer_context()
+        return self.serializer_class(*args, **kwargs)
+
+    def post(self, request):
+        try:
+            data = JSONParser().parse(request)
+            serializer = ProfileSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except JSONDecodeError:
+            return JsonResponse({"result": "error","message": "Json decoding error"}, status= 400)
+
+
+
+class ExpenseAPIView(views.APIView):
     serializer_class = ExpenseSerializer
+
+    def get_serializer_context(self):
+        return {
+            'request': self.request,
+            'format': self.format_kwarg,
+            'view': self
+        }
+
+    def get_serializer(self, *args, **kwargs):
+        kwargs['context'] = self.get_serializer_context()
+        return self.serializer_class(*args, **kwargs)
+
+    def post(self, request):
+        try:
+            data = JSONParser().parse(request)
+            serializer = ExpenseSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except JSONDecodeError:
+            return JsonResponse({"result": "error","message": "Json decoding error"}, status= 400)
