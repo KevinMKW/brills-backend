dc4a68e3ed6981097b11088233113508421139fe
KevinMKW <mkwkvn@gmail.com>
2023-12-31 18:23:54 +0300

    update urls to include target by id

--- brills_backend/urls.py
+++ brills_backend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProfileAPIView, BillAPIView
+from .views import ProfileAPIView, Profile, BillAPIView, Bill
 
 urlpatterns = [
     path('profile/', ProfileAPIView.as_view()),
+    path('profile/<uuid:pk>/', Profile.as_view()),
     path('bill/', BillAPIView.as_view()),
+    path('bill/<uuid:pk>/', Bill.as_view()),
 ]
