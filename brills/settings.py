ea9ff2ef78da4c866fe47bf4f3b6dd659a12ec2b
KevinMKW <mkwkvn@gmail.com>
2024-01-15 13:48:41 +0300

    Add allowed hosts

--- brills/settings.py
+++ brills/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5a1q7znmk=#8&6#1k9+2+958%!5(sw2#$bu!tsnw_2s=lpm3ps'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'keinmuriuki.pythonanywhere.com']
 
 # ADD the origin of your React app
 # CORS_ALLOWED_ORIGINS = [
